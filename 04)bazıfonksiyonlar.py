0faf75b2016134715c78a401be12e5b044c9c357
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-10-29 14:50:03 +0300

    Update 04)bazıfonksiyonlar.py

--- 04)bazıfonksiyonlar.py
+++ 04)bazıfonksiyonlar.py
@@ -1,1 +1,3 @@
-type() fonksiyonu içine gönderilen değerin hangi veri tipinden olduğunu söyler.
+print() --> ekrana bir değer bastırmak istersek print fonksiyonunu kullanırız.
+type() --> fonksiyonu içine gönderilen değerin hangi veri tipinden olduğunu söyler.
+format() --> print("{} + {} 'nin toplamı {} 'dır".format(a,b,a+b))
