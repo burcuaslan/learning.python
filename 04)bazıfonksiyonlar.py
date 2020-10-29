5f409254ce844c633b9a475268903277d0d5ebec
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-10-29 17:32:21 +0300

    Update 04)bazıfonksiyonlar.py

--- 04)bazıfonksiyonlar.py
+++ 04)bazıfonksiyonlar.py
@@ -1,3 +1,4 @@
 print() --> ekrana bir değer bastırmak istersek print fonksiyonunu kullanırız.
 type() --> fonksiyonu içine gönderilen değerin hangi veri tipinden olduğunu söyler.
 format() --> print("{} + {} 'nin toplamı {} 'dır".format(a,b,a+b))
+input() --> Pythonda kullanıcıdan girdi almamızı sağlayan fonksiyondur.
