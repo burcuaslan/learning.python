373196e6c82404ecdcd999e6bcbac28a8ef7fb10
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-10-29 14:38:07 +0300

    Update 01)notlar.py

--- 01)notlar.py
+++ 01)notlar.py
@@ -9,3 +9,28 @@
 **  --> üs bulma
 -   --> işaret değiştirme
 """
+
+
+#Eğer bir stringin başına * işareti koyup, print fonksiyonuna gönderirsek
+#bu string karakterlerine ayrılacak ve her bir karakter ayrı birer string olarak davranılarak ekrana basılacaktır.
+
+# Varsayılan olarak karakterlerin arasına boşluk konuluyor.
+print(*"Python")
+print(*"Python",sep = "\n")
+print(*"TBMM",sep =".")
+ 
+"""
+Çıktı
+ 
+P y t h o n
+ 
+P
+y
+t
+h
+o
+n
+ 
+T.B.M.M
+ 
+"""
