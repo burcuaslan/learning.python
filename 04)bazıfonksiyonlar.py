2adb772f55e5be58c63dcd3e02a45c836020bd7d
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-10-31 15:17:14 +0300

    Update 04)bazıfonksiyonlar.py

--- 04)bazıfonksiyonlar.py
+++ 04)bazıfonksiyonlar.py
@@ -2,3 +2,4 @@ print() --> ekrana bir değer bastırmak istersek print fonksiyonunu kullanırı
 type() --> fonksiyonu içine gönderilen değerin hangi veri tipinden olduğunu söyler.
 format() --> print("{} + {} 'nin toplamı {} 'dır".format(a,b,a+b))
 input() --> Pythonda kullanıcıdan girdi almamızı sağlayan fonksiyondur.
+range() --> Bu fonksiyon başlangıç, bitiş ve opsiyonel olarak artırma değeri alarak listelere benzeyen bir sayı dizisi oluşturur.
