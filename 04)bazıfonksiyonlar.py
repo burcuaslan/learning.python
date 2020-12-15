f23d42beed91998734942f57a202e9c8fcc2372c
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-12-15 20:23:56 +0300

    Update 04)bazıfonksiyonlar.py

--- 04)bazıfonksiyonlar.py
+++ 04)bazıfonksiyonlar.py
@@ -4,3 +4,4 @@ format() --> print("{} + {} 'nin toplamı {} 'dır".format(a,b,a+b))
 input() --> Pythonda kullanıcıdan girdi almamızı sağlayan fonksiyondur.
 range() --> Bu fonksiyon başlangıç, bitiş ve opsiyonel olarak artırma değeri alarak listelere benzeyen bir sayı dizisi oluşturur.
 while() --> While döngüleri belli bir koşul sağlandığı sürece bloğundaki işlemleri gerçekleştirmeye devam eder.
+liste.append(i) --> Listeye i elemanlarını ekleme
